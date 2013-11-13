60f222e0830464a1222b569372886f149bcc94be
Luke BrownGold <victorsigma@gmail.com>
2013-11-13 08:39:43 -0500

    Create laravel service provider

---
+++ src/VictorSigma/RedCard/RedCardServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace VictorSigma\RedCard;
+
+use Illuminate\Support\ServiceProvider;
+
+class RedCardServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
